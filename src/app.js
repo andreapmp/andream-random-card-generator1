efec36e8b98c27c3ab9b6e9dc34f8e649436648f
andreapmp <162078117+andreapmp@users.noreply.github.com>
2024-04-24 05:55:04 +0000

    First commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,57 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let suit = ["♦", "♥", "♠", "♣"];
+  let value = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "A",
+    "J",
+    "Q",
+    "K"
+  ];
+
+  // This function will select a suit and value from each array
+  let selectSuit = () => Math.floor(Math.random() * suit.length);
+  let selectValue = () => Math.floor(Math.random() * value.length);
+
+  function buildSuit() {
+    let generateSuit = suit[selectSuit()];
+
+    if (generateSuit === "♦" || generateSuit === "♥")
+      return `<div class="generate-suit" style="color:red">${generateSuit}</div>`;
+
+    return `<div class="generate-suit">${generateSuit}</div>`;
+  }
+
+  function buildValue() {
+    let generateValue = value[selectValue()];
+    return generateValue;
+  }
+
+  function drawCard() {
+    let suit = buildSuit();
+    let value = buildValue();
+
+    document.querySelector(".top").innerHTML = suit;
+    document.querySelector(".value").innerHTML = value;
+    document.querySelector(".bottom").innerHTML = suit;
+  }
+
+  function displayCard() {
+    document.querySelector(".card").innerHTML = `<div class="top"></div>
+    <div class="value"></div>
+    <div class="bottom"></div>`;
+
+    drawCard();
+  }
+
+  displayCard();
 };
